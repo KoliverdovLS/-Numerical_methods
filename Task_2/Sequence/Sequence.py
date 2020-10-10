d66a705787a7bf89f4c0e062bca4a68997f64461
KoliverdovLS <koliverdovls@sgu.ru>
2020-10-10 17:23:55 +0400

    feat: complate sequence task

---
+++ Task_2/Sequence/Sequence.py
@@ -0,0 +1,49 @@
+import math
+
+
+def sum_sequence1(n):
+    i = 1
+    result = 0
+    while i <= n:
+        result += 2 * i - 1
+        i += 1
+    return result
+
+def sum_sequence2(n):
+    i = 1
+    result = 0
+    while i <= n:
+        result += 2 ** i - 1
+        i += 1
+    return result
+
+def sum_sequence3(n):
+    i = 1
+    result = 0
+    while i <= n:
+        result += i ** 3 + 1
+        i += 1
+    return result
+
+
+print('Выбирите последовательность и введите номер, соответствующий нужной послежоватеьлности: \n 1 : 2n-1 \n 2 : 2^n-1 \n 3 : n^3 + 1')
+sequence = str(input())
+
+if 1 > int(sequence) > 3:
+    print('Error')
+else:
+    print('Введите n для вывода суммы элементов данной последовательности от 1 до n:')
+    n = int(input())
+
+    if n < 1:
+        print('Error')
+    else:
+        if sequence == '1':
+            print(sum_sequence1(n))
+        elif sequence == '2':
+            print(sum_sequence2(n))
+        elif sequence == '3':
+            print(sum_sequence3(n))
+
+
+
